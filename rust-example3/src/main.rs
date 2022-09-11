1d2b761140b3a585294f1c331f8b08a796689508
Nazorine <yliunssept@163.com>
2022-09-11 12:23:59 +0800

    0.0.4

--- rust-example3/src/main.rs
+++ rust-example3/src/main.rs
@@ -5,5 +5,30 @@ use peroxide::fuga::*;
 
 
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+
+    let x1 = 1;
+    let x2 = x1;  // 简单数据类型存在栈上，自动copy
+    let x3 = x1;
+    x1.print();
+    x2.print();
+    x3.print();
+
+    let vx1 = [1,2,3]; 
+    let vx2 = vx1;  // 简单数据类型(scalar、list)存在栈上，自动copy
+    println!("{:?}",vx1);
+    println!("{:?}",vx2);
+
+    let vx3 = vec![1,2,3];  // 复杂数类型(vector)
+    let vx4 = vx3;          // 使用后所有权移交至新的变量
+    // println!("{:?}",vx3);          // 这里不能再使用vx3 
+    println!("{:?}",vx4);
+
+    let vx5 = vec![4,5,6];
+    let vx6 = &vx5; 
+    // let vx7 = *vx6;
+    vx5.print();
+    vx6.print();
+    println!("{:?}",*vx6);
+
 }
